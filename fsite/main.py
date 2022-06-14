88919148ac34554dd97c8d74d5bc6cbc98c9f886
Nikita-Goncharov <nikitagoncarov657@gmail.com>
2022-06-14 21:55:57 +0300

    commit_0003

--- fsite/main.py
+++ fsite/main.py
@@ -1,6 +1,6 @@
 import os
 from PIL import Image
-from flask import Flask, abort, render_template, request, flash, redirect, url_for, session, send_file
+from flask import Flask, render_template, request, flash, redirect, url_for, session, send_file
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = 'static/upload/'
@@ -10,18 +10,20 @@ app.config['SECRET_KEY'] = 'orehgldgd39432lkgjdlghdl4355844bdlld'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-def delete():
+def img_path():
     img = os.listdir(UPLOAD_FOLDER)[0]
     imgpath = os.path.join(UPLOAD_FOLDER, img)
-    os.remove(imgpath)
+    return imgpath
+
+
+def delete():
+    os.remove(img_path())
 
 
 def rotate(angle):
-    img = os.listdir(UPLOAD_FOLDER)[0]
-    imgpath = os.path.join(UPLOAD_FOLDER, img)
-    im = Image.open(imgpath)
+    im = Image.open(img_path())
     im_rot = im.rotate(int(angle), expand=True)
-    im_rot.save(imgpath)
+    im_rot.save(img_path())
 
 
 
@@ -32,26 +34,20 @@ def home():
 
 @app.route('/download')
 def download():
-    return render_template('download.html', )
+    img_file_path = session.get('uploaded_img_path', None)
+    path = os.listdir(UPLOAD_FOLDER)
+    return render_template('download.html', user_image=img_file_path, path=path)
 
 
 @app.route('/download_img')
 def download_img():
-    img = os.listdir(UPLOAD_FOLDER)[0]
-    imgpath = os.path.join(UPLOAD_FOLDER, img)
-    return send_file(imgpath, as_attachment=True)
+    return send_file(img_path(), as_attachment=True)
 
 
 @app.route('/del')
 def delete_img():
     delete()
     return redirect(url_for('home'))
-
-
-# @app.route('/rotate_left')
-# def rotate_left_img():
-#     rotate(90)
-#     return redirect(url_for('flip'))
 
 
 @app.route('/rotate/<string:angle>')
@@ -84,11 +80,11 @@ def upload():
             img.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             session['uploaded_img_path'] = os.path.join(
                 app.config['UPLOAD_FOLDER'], filename)
-            return redirect(url_for('download'))
+            return redirect(url_for('flip'))
         else:
             flash('Delete the old file first')
     return render_template('upload.html')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True) # host='0.0.0.0', port=5000,
